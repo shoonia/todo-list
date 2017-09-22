3ede91a3a26e27c53225b4c97a6714aed5793a39
Zaytsev Sasha <zaytsev126@gmail.com>
2017-09-22 11:13:53 +0300

    Added item-for-task component

---
+++ src/components/ItemForTask.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+
+  div.list-group-item.mb-1 {{ id }} {{ text }} {{ done }}
+
+</template>
+
+<script>
+  const ItemForTask = Vue.component( 'item-for-task', {
+    props: [ 'id', 'text', 'done' ]
+  });
+
+  export default ItemForTask;
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,33 @@
-<template>
-  <h1>Hello</h1>
+<template lang="pug">
+
+  div
+    form.mb-2
+      div.input-group
+        input.form-control
+        div.input-group-btn
+          button.btn.btn-outline-dark Add
+
+    div(v-for="t in tasks").list-group
+      item-for-task(:id="t.id", :text="t.text", :done="t.done")
+
 </template>
 
 <script>
-  const TodoList = Vue.component( 'to-do-list', {
+  import ItemForTask from './ItemForTask.vue';
 
+  const TodoList = Vue.component( 'to-do-list', {
+    components: {
+      ItemForTask
+    },
+
+    data () {
+      return {
+        tasks: [
+          { id: 0, text: 'test', done: false },
+          { id: 1, text: 'next', done: false }
+        ]
+      }
+    }
   });
 
   export default TodoList;
